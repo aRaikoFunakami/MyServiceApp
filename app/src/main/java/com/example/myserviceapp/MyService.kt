c10a854a24a5e8f5236e1bb81e5c163cdc9be846
aRaiko.Funakami <raiko.funakami@access-company.com>
2024-04-02 18:23:54 +0900

    add network function to communicate Copilot server

--- app/src/main/java/com/example/myserviceapp/MyService.kt
+++ app/src/main/java/com/example/myserviceapp/MyService.kt
@@ -12,6 +12,9 @@ import androidx.core.app.NotificationCompat
 import android.util.Log
 import android.os.Handler
 import android.os.Looper
+import android.media.RingtoneManager
+import android.media.AudioManager
+import android.net.Uri
 
 import android.graphics.PixelFormat
 import android.view.Gravity
@@ -24,6 +27,22 @@ import android.speech.SpeechRecognizer
 import android.speech.RecognitionListener
 
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+import retrofit2.Call
+import kotlin.math.abs
+
+interface ApiService {
+    @GET("input")
+    fun getTextResponse(@Query("text") text: String): Call<String>
+}
+
+
 // 通知チャネルのID
 private const val CHANNEL_ID = "my_channel_id"
 
@@ -34,6 +53,8 @@ class MyService : Service() {
     private var speechRecognizer: SpeechRecognizer? = null
     private var isConversationMode = false
     var isAIProcessingConversation = false // 会話処理中かどうか
+    private lateinit var audioManager: AudioManager
+
 
     override fun onBind(intent: Intent): IBinder? {
         return null
@@ -53,6 +74,9 @@ class MyService : Service() {
         val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
         notificationManager.createNotificationChannel(channel)
 
+        // 音声認識中の音声を消去するためのトリック用
+        audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+
         initializeSpeechRecognizer()
     }
 
@@ -62,12 +86,11 @@ class MyService : Service() {
             Log.d(TAG, "Service is starting...")
             // サービスの初期化や前景処理の開始
             startForegroundService()
-            showOverlayImage() // オーバーレイ表示のセットアップを呼び出し
+            // showOverlayImage() // オーバーレイ表示のセットアップを呼び出し
         } else {
             Log.d(TAG, "Service is already running.")
             // 既にサービスが起動している場合の追加の処理
-            val action = intent?.action
-            when(action) {
+            when(intent?.action) {
                 "ACTION_SHOW_OVERLAY" -> showOverlayImage()  // オーバーレイを表示
                 "ACTION_HIDE_OVERLAY" -> hideOverlayImage()  // オーバーレイを非表示
                 "ACTION_TOGGLE_OVERLAY" -> toggleOverlayVisibility()  // オーバーレイの表示状態を切り替え
@@ -127,6 +150,17 @@ class MyService : Service() {
         }
     }
 
+    private fun playNotificationSound() {
+        try {
+            val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+            //val notification: Uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+            val ringtone = RingtoneManager.getRingtone(applicationContext, notification)
+            ringtone.play()
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
     private fun matchStartKeyWord(input: String): Boolean {
         // 正規表現用のパターン文字列をリストで管理
         // ハロー アクセス で呼び出したいが間違って認識した場合も許容する
@@ -134,6 +168,7 @@ class MyService : Service() {
         val patterns = listOf(
             "(\\\\s|^)ハロー\\s*アクセス(\\\\s|\$)",
             "(\\\\s|^)ハロー\\s*学生(\\\\s|\$)",
+            "(\\\\s|^)ハロー\\s*ワークです(\\\\s|\$)",
             "(\\\\s|^)ハロー\\s*ワークスです(\\\\s|\$)",
             "(\\\\s|^)ハロー\\s*ワークス(\\\\s|\$)",
             "(\\\\s|^)ハロー\\s*惑星(\\\\s|\$)",
@@ -151,13 +186,30 @@ class MyService : Service() {
             setRecognitionListener(object : RecognitionListenerAdapter(this@MyService) {
                 override fun onResults(results: Bundle) {
                     Log.d(TAG, "onResults")
+                    unmuteBeepForRecognizer()
+
                     val matches = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
                     matches?.let {
                         if (it.isNotEmpty()) {
                             val text = it[0]
                             Log.d(TAG, "Recognized text: $text")
-                            if (matchStartKeyWord(text)) {
+
+                            if (!isConversationMode && matchStartKeyWord(text)) {
+                                // AIとの会話モードに入る
+                                playNotificationSound() // 通知音を再生
                                 setConversationMode(true)
+                                Log.d(TAG, "isAIProcessingConversation: $isAIProcessingConversation")
+                                Log.d(TAG, "isConversationMode mode: $isConversationMode")
+                                startAIConversationProcessing(text)
+                            } else if (isConversationMode) {
+                                // AIと会話中
+                                Log.d(TAG, "isAIProcessingConversation: $isAIProcessingConversation")
+                                Log.d(TAG, "isConversationMode mode: $isConversationMode")
+                                startAIConversationProcessing(text)
+                            } else {
+                                // AIと会話を始める前
+                                // No action
+                                Log.d(TAG, "isAIProcessingConversation: $isAIProcessingConversation")
                                 Log.d(TAG, "isConversationMode mode: $isConversationMode")
                             }
                         }
@@ -194,8 +246,17 @@ class MyService : Service() {
         }
         startListening()
     }
+
+    private fun muteBeepForRecognizer() {
+        audioManager.adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_MUTE, 0)
+    }
+    private fun unmuteBeepForRecognizer() {
+        audioManager.adjustStreamVolume(AudioManager.STREAM_NOTIFICATION, AudioManager.ADJUST_UNMUTE, 0)
+    }
     private fun startListening() {
         Log.d(TAG, "StartListening!!!")
+        muteBeepForRecognizer()
+
         val lang = "ja-JP"
         val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
             putExtra(RecognizerIntent.EXTRA_LANGUAGE, lang)
@@ -209,13 +270,40 @@ class MyService : Service() {
     fun setConversationMode(active: Boolean) {
         isConversationMode = active
         Log.d(TAG, "Conversation mode set to: $isConversationMode")
+        if (isConversationMode) {
+            showOverlayImage()
+        } else {
+            hideOverlayImage()
+        }
     }
-    fun startConversationProcessing() {
+    fun startAIConversationProcessing(requestText: String) {
         isAIProcessingConversation = true
         // 会話処理の開始
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val retrofit = Retrofit.Builder()
+                    .baseUrl("http://192.168.1.100:8000/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+
+                val service = retrofit.create(ApiService::class.java)
+                val response = service.getTextResponse(requestText).execute()
+
+                if (response.isSuccessful) {
+                    val responseData = response.body()
+                    Log.d(TAG, "Received data: $responseData")
+                } else {
+                    Log.d(TAG, "Failed to receive data")
+                }
+            } catch (e: Exception) {
+                Log.e(TAG, "Error during network call", e)
+            } finally {
+                finishAIConversationProcessing()
+            }
+        }
     }
 
-    fun finishConversationProcessing() {
+    private fun finishAIConversationProcessing() {
         isAIProcessingConversation = false
         // 会話処理の終了
     }
@@ -224,7 +312,7 @@ class MyService : Service() {
         super.onDestroy()
         isRunning = false
         Log.d(TAG, "Service onDestroy")
-        stopForeground(true)
+        stopForeground(STOP_FOREGROUND_REMOVE)
 
         speechRecognizer?.stopListening() // SpeechRecognizerのリスニングを停止
         speechRecognizer?.destroy() // SpeechRecognizerのリソースを解放
@@ -239,31 +327,25 @@ class MyService : Service() {
 abstract class RecognitionListenerAdapter(private val service: MyService) : RecognitionListener {
     private val TAG = "RecognitionListener"
     private val silenceThreshold = 4.0f // RMS dBの閾値。この値以下を無音とみなす
-    private val silenceTimeout = 10000L // 無音状態がこの時間（ミリ秒）続いたらタイムアウトとする
+    private val silenceTimeout = 5000L // 無音状態がこの時間（ミリ秒）続いたらタイムアウトとする
     private var isCurrentlySilent = false // 現在無音状態かどうか
 
     private val silenceHandler = Handler(Looper.getMainLooper())
-    private val checkSilenceRunnable: Runnable = object : Runnable {
-        override fun run() {
-            // 一定時間無音状態が続いたときの処理
-            Log.d("SpeechRecognizer", "無音状態が${silenceTimeout}ミリ秒以上続きました。")
+    private val checkSilenceRunnable: Runnable = Runnable {
+        // 一定時間無音状態が続いたときの処理
+        Log.d("SpeechRecognizer", "無音状態が${silenceTimeout}ミリ秒以上続きました。")
 
-            // 会話のためにAIが処理を進めている場合には無音時間が続いても無視する
-            // AI処理が終了してから無音状態が一定時間続いた場合のみ会話モードを終了
-            if (!service.isAIProcessingConversation)
-            {
-                service.setConversationMode(false)
-            }
-            // 条件に関わらず、一定時間後に再度チェックを行うようスケジュール
-            // これにより、checkSilenceRunnableの定期的な実行が保証される
-            silenceHandler.postDelayed(this, silenceTimeout)
-            isCurrentlySilent = false // 無音状態をリセット
+        // 会話のためにAIが処理を進めている場合には無音時間が続いても無視する
+        // AI処理が終了してから無音状態が一定時間続いた場合のみ会話モードを終了
+        if (!service.isAIProcessingConversation) {
+            service.setConversationMode(false)
         }
+        isCurrentlySilent = false // 無音状態をリセット
     }
 
     override fun onRmsChanged(rmsdB: Float) {
         //Log.d(TAG, "onRmsChanged: 受信音声のRMS変化が検出されました。RMS dB: $rmsdB")
-        if (Math.abs(rmsdB) < silenceThreshold) {
+        if (abs(rmsdB) < silenceThreshold) {
             if (!isCurrentlySilent) {
                 // 無音状態の開始を検出
                 isCurrentlySilent = true
